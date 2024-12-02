64813d45490dfa7de67beb310628a13a1ddc8545
willson <q2382285@gmail.com>
2024-12-02 17:59:33 +0800

    add click event

--- src/lib/CountGamePlace.svelte
+++ src/lib/CountGamePlace.svelte
@@ -1,92 +1,41 @@
+<script>
+    import {countGame} from '../stores/store.js';
+    const diceImages = [
+        './dice0.svg',
+        './dice1.svg',
+        './dice2.svg',
+        './dice3.svg',
+        './dice4.svg',
+        './dice5.svg'
+    ];
+
+    const numbers = [0, 1, 2, 3, 4, 5, 6];
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="">
     <!-- bet count place -->
     <div class="grid grid-cols-1 gap-1">
-      <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
-        <!-- 子元素共享樣式 -->
-        <div class="aspect-square flex justify-center items-center">
-            <img src="./dice-one.svg" alt="Increment" class="" />
-        </div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">0</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">1</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">2</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">3</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">4</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">5</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">6</div>
-      </div>
-      <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
-        <!-- 子元素共享樣式 -->
-        <div class="aspect-squareflex justify-center items-center">
-            <img src="./dice-two.svg" alt="Increment" class="" />
-        </div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">0</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">1</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">2</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">3</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">4</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">5</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">6</div>
-      </div>
-      <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
-        <!-- 子元素共享樣式 -->
-        <div class="aspect-square flex justify-center items-center">
-            <div class="aspect-squareflex justify-center items-center">
-                <img src="./dice-three.svg" alt="Increment" class="" />
+        {#each diceImages as diceImage,diceIndex}
+        <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
+            <!-- 子元素共享樣式 -->
+            <div class="aspect-square flex justify-center items-center">
+                <img src={diceImage} alt="Increment" class="" />
             </div>
+            {#each numbers as number,numberIndex}
+                
+                <div on:click={()=>{
+                    if($countGame[diceIndex] == numberIndex){
+                        $countGame[diceIndex] = 7;
+                    }else{
+                        $countGame[diceIndex] = numberIndex;
+                    }
+                        
+                    
+                }} class="aspect-square border border-gray-500 rounded-md flex justify-center items-center {$countGame[diceIndex] == number?"bg-red-800":""}">{number}</div>
+            {/each}
         </div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">0</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">1</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">2</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">3</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">4</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">5</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">6</div>
-      </div>
-      <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
-        <!-- 子元素共享樣式 -->
-        <div class="aspect-square flex justify-center items-center">
-            <div class="aspect-squareflex justify-center items-center">
-                <img src="./dice-four.svg" alt="Increment" class="" />
-            </div>
-        </div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">0</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">1</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">2</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">3</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">4</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">5</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">6</div>
-      </div>
-      <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
-        <!-- 子元素共享樣式 -->
-        <div class="aspect-square flex justify-center items-center">
-            <div class="aspect-squareflex justify-center items-center">
-                <img src="./dice-five.svg" alt="Increment" class="" />
-            </div>
-        </div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">0</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">1</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">2</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">3</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">4</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">5</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">6</div>
-      </div>
-      <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
-        <!-- 子元素共享樣式 -->
-        <div class="aspect-square flex justify-center items-center">
-            <div class="aspect-squareflex justify-center items-center">
-                <img src="./dice-six.svg" alt="Increment" class="" />
-            </div>
-        </div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">0</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">1</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">2</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">3</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">4</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">5</div>
-        <div class="aspect-square border border-gray-500 rounded-md flex justify-center items-center">6</div>
-      </div>
+    {/each}
       
       
     </div>
--- src/lib/IconCountPlace.svelte
+++ src/lib/IconCountPlace.svelte
@@ -1,53 +1,33 @@
 <script>
     import PadingLayout from "./components/PadingLayout.svelte";
+    import { iconState } from "../stores/store.js";
+
+    const icons = [
+        { src: './dice0.svg', state: 0 },
+        { src: './dice1.svg', state: 1 },
+        { src: './dice2.svg', state: 2 },
+        { src: './dice3.svg', state: 3 },
+        { src: './dice4.svg', state: 4 },
+        { src: './dice5.svg', state: 5 }
+    ];
 </script>
 
 <PadingLayout>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div
-        class="grid grid-cols-3 gap-1 text-center text-green-200 border border-yellow-400 rounded-xl text-lg font-bold"
+        class="grid grid-cols-3 gap-1 text-center text-green-200 border border-yellow-400 rounded-xl overflow-hidden text-lg font-bold"
     >
         <!-- 子元素共享樣式 -->
-        <div class="rounded-md flex justify-center items-center">
-            <img
-                src="./dice-one.svg"
-                alt="Increment"
-                class="w-full max-w-[50px] max-h-[50px]"
-            />
-        </div>
-        <div class="rounded-md flex justify-center items-center">
-            <img
-                src="./dice-two.svg"
-                alt="Increment"
-                class="w-full max-w-[50px] max-h-[50px]"
-            />
-        </div>
-        <div class="rounded-md flex justify-center items-center">
-            <img
-                src="./dice-three.svg"
-                alt="Increment"
-                class="w-full max-w-[50px] max-h-[50px]"
-            />
-        </div>
-        <div class="rounded-md flex justify-center items-center">
-            <img
-                src="./dice-four.svg"
-                alt="Increment"
-                class="w-full max-w-[50px] max-h-[50px]"
-            />
-        </div>
-        <div class="rounded-md flex justify-center items-center">
-            <img
-                src="./dice-five.svg"
-                alt="Increment"
-                class="w-full max-w-[50px] max-h-[50px]"
-            />
-        </div>
-        <div class="rounded-md flex justify-center items-center">
-            <img
-                src="./dice-six.svg"
-                alt="Increment"
-                class="w-full max-w-[50px] max-h-[50px]"
-            />
-        </div>
+        {#each icons as icon,index}
+            <div on:click={() => {
+                $iconState = icon.state;
+            }} class="rounded-md flex justify-center items-center {$iconState == index?"bg-red-800":""}">
+                <img
+                    src={icon.src}
+                    alt="Increment"
+                    class="w-full max-w-[50px] max-h-[50px]"
+                />
+            </div>
+        {/each}
     </div>
 </PadingLayout>
