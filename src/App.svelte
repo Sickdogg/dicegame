ff0a1a50d65b0b5a8229e0a0e808d4431d7a5757
willson <q2382285@gmail.com>
2024-12-02 11:05:06 +0800

    delete no use

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,101 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
+<div class="w-[100dvw] h-[100dvh] bg-green-900 p-1">
+  <!-- Grid Layout -->
 
-<main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <!-- bet count place -->
+    <div class="grid grid-cols-1 gap-1">
+      <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
+        <!-- 子元素共享樣式 -->
+        <div class="aspect-square bg-green-800 flex justify-center items-center">1</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">0</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">1</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">2</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">3</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">4</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">5</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">6</div>
+      </div>
+      <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
+        <!-- 子元素共享樣式 -->
+        <div class="aspect-square bg-green-800 flex justify-center items-center">2</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">0</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">1</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">2</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">3</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">4</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">5</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">6</div>
+      </div>
+      <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
+        <!-- 子元素共享樣式 -->
+        <div class="aspect-square bg-green-800 flex justify-center items-center">3</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">0</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">1</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">2</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">3</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">4</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">5</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">6</div>
+      </div>
+      <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
+        <!-- 子元素共享樣式 -->
+        <div class="aspect-square bg-green-800 flex justify-center items-center">4</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">0</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">1</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">2</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">3</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">4</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">5</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">6</div>
+      </div>
+      <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
+        <!-- 子元素共享樣式 -->
+        <div class="aspect-square bg-green-800 flex justify-center items-center">5</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">0</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">1</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">2</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">3</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">4</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">5</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">6</div>
+      </div>
+      <div class="grid grid-cols-8 gap-1 text-center text-green-200 text-lg font-bold">
+        <!-- 子元素共享樣式 -->
+        <div class="aspect-square bg-green-800 flex justify-center items-center">6</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">0</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">1</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">2</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">3</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">4</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">5</div>
+        <div class="aspect-square bg-green-700 flex justify-center items-center">6</div>
+      </div>
+      
+      
+    </div>
+    
+    <!-- number of bet -->
+    <div class="grid grid-cols-8 gap-1 text-center text-[0.7rem]">
+      <div class=" text-yellow-400 font-bold">1:X</div>
+      <div class=" text-green-200">2.5</div>
+      <div class=" text-green-200">2</div>
+      <div class=" text-green-200">4</div>
+      <div class=" text-green-200">15</div>
+      <div class=" text-green-200">100</div>
+      <div class=" text-green-200">1250</div>
+      <div class=" text-green-200">40000</div>
+    </div>
   </div>
-  <h1>Vite + Svelte + PWA</h1>
+  
 
-  <div class="card">
-    <Counter />
+  <div class="grid grid-cols-4 gap-1">
+    <div class="aspect-[1/1] border border-black">123</div>
+    <div class="aspect-[1/1] border border-black">123</div>
+    <div class="aspect-[1/1] border border-black">123</div>
+    <div class="aspect-[1/1] border border-black">123</div>
   </div>
+  <!-- end -->
+</div>
 
-  <p>
-    <a href="https://github.com/shinokada/vite-svelte-pwa" target="_blank" rel="noreferrer">GitHub Repo</a>
-  </p>
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
