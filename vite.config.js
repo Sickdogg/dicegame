82d27f5eb4aad2dbd918e72a82955d5e6f16dae5
willson <q2382285@gmail.com>
2024-12-02 09:22:41 +0800

    add base:""

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base:"",
   build: {
     assetsInlineLimit: 0, // 0 means all files will be inlined
   },
