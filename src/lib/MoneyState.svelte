be420737ef002e5d0afcfbe42b691bfcc325ef1f
willson <q2382285@gmail.com>
2024-12-09 15:48:51 +0800

    fix money state

--- src/lib/MoneyState.svelte
+++ src/lib/MoneyState.svelte
@@ -1,14 +1,8 @@
-<div class="grid grid-cols-8 w-full">
+<div class="grid grid-cols-1 w-full">
     <div
       class="col-span-4 flex justify-center items-center text-white"
     >
       <p>USDT:</p>
       <p class="text-yellow-500">USDT</p>
-    </div>
-    <div
-      class="col-span-4 flex justify-center items-center text-white"
-    >
-      <p>BET:</p>
-      <p class="text-yellow-500">$$$</p>
     </div>
   </div>
